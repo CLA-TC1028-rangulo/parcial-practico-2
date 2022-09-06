6e32317c3a2426711b75c6298bc417c7289316f6
pangulotecmx <88519161+pangulotecmx@users.noreply.github.com>
2022-09-06 14:58:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![Tec de Monterrey](images/logotecmty.png)
+# Ejercicios con estatutos de repetición
+
+- 17 Número Cuadrado Mayor Que
+
---
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    numero = int(input("Escribe un numero : "))
+    #escribe tu código abajo de esta línea
+
+
+if __name__=='__main__':
+    main()
